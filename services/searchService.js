98cbdbf34629e23f6216675313f03b526d458c97
Kittisak Thanapaya <kittisak.ac.th@gmail.com>
2026-06-22 16:52:24 +0700

    Debug firestore docs

--- services/searchService.js
+++ services/searchService.js
@@ -3,9 +3,7 @@ const db = require("../config/firebase");
 async function searchDocuments(question) {
 
   const snapshot =
-    await db
-      .collection("documents")
-      .get();
+    await db.collection("documents").get();
 
   let context = "";
 
@@ -13,16 +11,25 @@ async function searchDocuments(question) {
 
     const data = doc.data();
 
-    if (!data.content) {
+    console.log(
+      "DOC:",
+      doc.id,
+      Object.keys(data)
+    );
+
+    if (
+      !data.content ||
+      typeof data.content !== "string"
+    ) {
       console.log(
-        "Missing content:",
+        "SKIP DOC:",
         doc.id
       );
       return;
     }
 
     context +=
-      `\n[${data.fileName || "Unknown"}]\n`;
+      `\n[${data.fileName || doc.id}]\n`;
 
     context +=
       data.content.substring(
