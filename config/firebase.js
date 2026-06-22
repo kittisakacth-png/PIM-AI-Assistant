102ae731f9226d4d61c1cc4d356cbe9319682a54
Kittisak Thanapaya <kittisak.ac.th@gmail.com>
2026-06-22 16:45:41 +0700

    Use Firebase env vars

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,26 @@
 const admin = require("firebase-admin");
+console.log(
+  "PROJECT:",
+  process.env.FIREBASE_PROJECT_ID
+);
 
-const serviceAccount =
-  require("../firebase-key.json");
+console.log(
+  "EMAIL:",
+  process.env.FIREBASE_CLIENT_EMAIL
+);
 
-admin.initializeApp({
-  credential:
-    admin.credential.cert(
-      serviceAccount
-    )
-});
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
+};
 
-const db =
-  admin.firestore();
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+
+const db = admin.firestore();
 
 module.exports = db;
